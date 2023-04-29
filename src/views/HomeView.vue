006c67144ab57efe430fcc8cfa1b341e82d41953
Donghoon <thdehdgns@gmail.com>
2023-04-29 17:28:32 +0900

    feat: 대화 시작 모달 추가 & input validation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,30 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+const dialog = ref(false);
+const topic = ref('');
+
+function openDialog() {
+    dialog.value = true;
+}
+
+function closeDialog() {
+    dialog.value = false;
+}
+
+function handleClickStartNewChatButton() {
+    openDialog()
+}
+
+function required(value: string) {
+    return !!value
+};
+
+function validateTopic() {
+    return required(topic.value)
+}
+
+
 </script>
 
 <template>
@@ -7,12 +33,38 @@
         <img class="mt-2"
              src="https://drpdbryplxlouwfdwkvh.supabase.co/storage/v1/object/public/assets/public/main-rubber-duck.png"
              alt="main-rubber-duck image"/>
-        <button class="bg-black hover:bg-gray-500 text-white font-bold py-4 rounded-full w-full mt-6">
+        <button class="bg-black hover:bg-gray-500 text-white font-bold py-4 rounded-full w-full mt-6"
+                @click="handleClickStartNewChatButton">
             새로운 대화 시작
         </button>
         <button class="bg-black hover:bg-gray-500 text-white font-bold py-4 rounded-full w-full mt-4">
             대화 목록
         </button>
+        <div class="text-center">
+            <v-dialog
+                    v-model="dialog"
+                    width="auto"
+            >
+                <v-card>
+                    <v-card-text>
+                        고무오리에게 들려줄 주제를 입력해주세요.
+                    </v-card-text>
+                    <v-text-field
+                            v-model="topic"
+                            :readonly="loading"
+                            class="mb-2"
+                            clearable
+                            placeholder="어떤 이야기를 들려주고 싶으신가요?"
+                    ></v-text-field>
+                    <v-card-actions>
+                        <v-btn color="primary" block :disabled="!validateTopic()">대화 시작하기</v-btn>
+                    </v-card-actions>
+                    <v-card-actions>
+                        <v-btn color="primary" block @click="closeDialog">닫기</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </div>
     </div>
 </template>
 
