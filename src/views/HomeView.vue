774487e6c1718b3ca128714d9bd76b41d8b5be48
Donghoon <thdehdgns@gmail.com>
2023-04-29 17:16:46 +0900

    feat: 고무오리 이미지 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@
 <template>
     <div>
         <h1 class="home-title">Quack Chat</h1>
+        <img class="mt-2"
+             src="https://drpdbryplxlouwfdwkvh.supabase.co/storage/v1/object/public/assets/public/main-rubber-duck.png"
+             alt="main-rubber-duck image"/>
     </div>
 </template>
 
