f4706e21f7842f9aed422d497b0faee820b88bcd
Donghoon <thdehdgns@gmail.com>
2023-04-29 17:17:13 +0900

    feat: 버튼 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,12 @@
         <img class="mt-2"
              src="https://drpdbryplxlouwfdwkvh.supabase.co/storage/v1/object/public/assets/public/main-rubber-duck.png"
              alt="main-rubber-duck image"/>
+        <button class="bg-black hover:bg-gray-500 text-white font-bold py-4 rounded-full w-full mt-6">
+            새로운 대화 시작
+        </button>
+        <button class="bg-black hover:bg-gray-500 text-white font-bold py-4 rounded-full w-full mt-4">
+            대화 목록
+        </button>
     </div>
 </template>
 
