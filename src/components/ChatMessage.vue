73ac44887d61f16d0de902dffe88bf3e61f8d08e
Donghoon <thdehdgns@gmail.com>
2023-05-01 15:45:36 +0900

    feat: add ChatMessage

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-[#00ced1] rounded-xl py-1 px-2 text-white">
+    {{ content }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Message } from '@/types/message'
+
+// TODO : String as () => Pick<Message, 'content'>이 왜 안되는지 알아보기
+defineProps({
+  content: {
+    type: String as unknown as () => Pick<Message, 'content'>,
+    required: true
+  }
+})
+</script>
+
+<style scoped></style>
