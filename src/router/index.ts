11aeafa025f828e292a7b38bccf540f96156a0ed
Donghoon <thdehdgns@gmail.com>
2023-05-20 23:07:20 +0900

    feat: Home에서 로그인 후 ChatList로 이동

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,30 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import ChatView from "@/views/ChatView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/HomeView.vue')
-        },
-        {
-            path: '/chat/:chatId',
-            name: 'chat',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/ChatView.vue')
-        },
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/chat/list',
+      name: 'chatList',
+      component: () => import('../views/ChatListView.vue')
+    },
+    {
+      path: '/chat/:chatId',
+      name: 'chat',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ChatView.vue')
+    }
+  ]
 })
 
 export default router
