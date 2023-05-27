bea22ce3a2017046bc9a237f39359bb745b834f6
Donghoon <thdehdgns@gmail.com>
2023-05-27 17:30:50 +0900

    feat: meta store 상태 및 핸들러 추가

--- src/stores/meta.ts
+++ src/stores/meta.ts
@@ -1,24 +1,43 @@
 import { defineStore } from 'pinia'
 
 export type RootState = {
+  showTopBar: boolean
   metaInfo: {
     title: string | null
+    showBackButton: boolean
   }
 }
+
+export const defaultMetaInfo = { title: '', showBackButton: true }
 
 export const useMetaStore = defineStore('meta', {
   state: () =>
     ({
-      metaInfo: {
-        title: ''
-      }
+      showTopBar: true,
+      metaInfo: defaultMetaInfo
     } as RootState),
   getters: {
-    getMetaTitle: (state) => state.metaInfo.title
+    getShowTopBar: (state) => state.showTopBar,
+    getMetaInfo: (state) => state.metaInfo,
+    getMetaTitle: (state) => state.metaInfo.title,
+    getShowBackButton: (state) => state.metaInfo.showBackButton
   },
   actions: {
+    setShowTopBar(showTopBar: boolean) {
+      this.showTopBar = showTopBar
+    },
+    setMetaInfo(metaInfo: RootState['metaInfo']) {
+      this.metaInfo = metaInfo
+    },
     setMetaTitle(title: string) {
       this.metaInfo.title = title
+    },
+    setShowBackButton(showBackButton: boolean) {
+      this.metaInfo.showBackButton = showBackButton
+    },
+    resetMeta() {
+      this.metaInfo = defaultMetaInfo
+      this.showTopBar = true
     }
   }
 })
