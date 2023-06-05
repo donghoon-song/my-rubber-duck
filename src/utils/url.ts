04486dcdaf9c33a805d3bab1f39dee325ec3ca4a
Donghoon <thdehdgns@gmail.com>
2023-06-05 15:17:37 +0900

    chore: 디버깅용 로그 추가

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,7 @@
 export const getURL = () => {
   let url = import.meta.env.VITE_PUBLIC_SITE_URL ?? 'http://localhost:3000'
+  console.info('getURL', url)
+  console.info('import.meta.env.VITE_PUBLIC_SITE_URL', import.meta.env.VITE_PUBLIC_SITE_URL)
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`
   return url
