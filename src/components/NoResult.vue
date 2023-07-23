3a85ed397d7d4c92c0f615b7c4ddf096c3c1cd3a
Donghoon <thdehdgns@gmail.com>
2023-07-23 18:07:18 +0900

    feat: NoResult 화면 추가

---
+++ src/components/NoResult.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col items-center">
+    <v-img :src="IMAGE_URL.RUBBER_DUCK_NO_RESULT" alt="empty talk image" width="90vw" />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IMAGE_URL } from '@/utils/constants/image.js'
+</script>
+
+<style scoped></style>
